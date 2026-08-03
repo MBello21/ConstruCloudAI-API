7b3fb343faf2f2dfbd612c00b711929da5183ba7
MBello21 <magarbello@gmail.com>
2026-08-03 16:14:14 +0000

    fix clientes

--- src/app/models/clientes.py
+++ src/app/models/clientes.py
@@ -2,7 +2,9 @@ from sqlalchemy import func, String, Text, ForeignKey, Integer, Numeric, DateTim
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import TYPE_CHECKING
 from ..database import Base
-from .presupuestos import Presupuestos
+
+if TYPE_CHECKING:
+    from .presupuestos import Presupuestos
 
 
 class Clientes(Base):
@@ -14,5 +16,11 @@ class Clientes(Base):
     poblacion: Mapped[str] = mapped_column(String(400), nullable=False)
     telefono: Mapped[str] = mapped_column(String(20), nullable=False)
     email: Mapped[str] = mapped_column(String(300), nullable=True)
+    estado: Mapped[str] = mapped_column(
+        String(50), nullable=True, default="Activo")
+    tipo: Mapped[str] = mapped_column(
+        String(50), nullable=True, default="Empresa")
+    cif: Mapped[str] = mapped_column(String(20), nullable=True)
+    notas: Mapped[str] = mapped_column(Text, nullable=True)
     presupuestos: Mapped[list["Presupuestos"]] = relationship(
         "Presupuestos", back_populates="cliente")
